c92aead2144126895e4008e24d406dc24582902b
streetmarker <streetmarker@users.noreply.github.com>
2022-01-28 00:16:58 +0100

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-card>
+    <v-tabs
+      color="deep-purple accent-4"
+      right
+    >
+      <v-tab>Landscape</v-tab>
+      <v-tab>City</v-tab>
+      <v-tab>Abstract</v-tab>
+
+      <v-tab-item
+        v-for="n in 3"
+        :key="n"
+      >
+        <v-container fluid>
+          <v-row>
+            <v-col
+              v-for="i in 6"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-img
+                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                aspect-ratio="1">
+                <template v-slot:placeholder>
+        <v-row
+          class="fill-height ma-0"
+          align="center"
+          justify="center"
+        >
+          <v-progress-circular
+            indeterminate
+            color="grey lighten-5"
+          ></v-progress-circular>
+        </v-row>
+      </template>
+              </v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
