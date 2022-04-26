78e5d264457f18b90953021494278ff9e957ccf8
Michał Jaroń <64842623+streetmarker@users.noreply.github.com>
2022-04-26 12:13:57 +0200

    first gdrive link

--- src/views/Portrety.vue
+++ src/views/Portrety.vue
@@ -3,276 +3,78 @@
 <template>
   <v-container
     id="dashboard-view"
-    fluid
     tag="section"
+    class="fill-height"
+    fluid
+    style="min-height: 434px"
   >
-    <img
-      src="https://storage.googleapis.com/example-bucket/cat.jpeg?X-Goog-Algorithm=
-GOOG4-RSA-SHA256&X-Goog-Credential=example%40example-project.iam.gserviceaccount
-.com%2F20181026%2Fus-central1%2Fstorage%2Fgoog4_request&X-Goog-Date=20181026T18
-1309Z&X-Goog-Expires=900&X-Goog-SignedHeaders=host&X-Goog-Signature=247a2aa45f16
-9edf4d187d54e7cc46e4731b1e6273242c4f4c39a1d2507a0e58706e25e3a85a7dbb891d62afa849
-6def8e260c1db863d9ace85ff0a184b894b117fe46d1225c82f2aa19efd52cf21d3e2022b3b868dc
-c1aca2741951ed5bf3bb25a34f5e9316a2841e8ff4c530b22ceaa1c5ce09c7cbb5732631510c2058
-0e61723f5594de3aea497f195456a2ff2bdd0d13bad47289d8611b6f9cfeef0c46c91a455b94e90a
-66924f722292d21e24d31dcfb38ce0c0f353ffa5a9756fc2a9f2b40bc2113206a81e324fc4fd6823
-a29163fa845c8ae7eca1fcf6e5bb48b3200983c56c5ca81fffb151cca7402beddfc4a76b13344703
-2ea7abedc098d2eb14a7"
-      alt=""
-    >
-    <!-- <iframe
-      src="https://giphy.com/embed/VGuAZNdkPUpEY"
-      width="480"
-      height="251"
-      frameBorder="0"
-      class="giphy-embed"
-      allowFullScreen
-    ></iframe>
-    <p>
-      <a href="https://giphy.com/gifs/meme-nyan-cat-VGuAZNdkPUpEY">via GIPHY</a>
-    </p> -->
-    <!-- <v-row>
-      <v-col cols="12">
-        <v-row>
-          <v-col
-            v-for="(chart, i) in charts"
-            :key="`chart-${i}`"
-            cols="12"
-            md="6"
-            lg="4"
-          >
-            <material-chart-card
-              :color="chart.color"
-              :data="chart.data"
-              :options="chart.options"
-              :responsive-options="chart.responsiveOptions"
-              :title="chart.title"
-              :type="chart.type"
-            >
-              <template #subtitle>
-                <div class="font-weight-light text--secondary">
-                  <div v-html="chart.subtitle" />
-                </div>
-              </template>
-
-              <template #actions>
-                <v-icon
-                  class="mr-1"
-                  small
-                >
-                  mdi-clock-outline
-                </v-icon>
-
-                <span
-                  class="text-caption grey--text font-weight-light"
-                  v-text="chart.time"
-                />
-              </template>
-            </material-chart-card>
-          </v-col>
-        </v-row>
-      </v-col>
-
-      <v-col
-        v-for="({ actionIcon, actionText, ...attrs }, i) in stats"
-        :key="i"
-        cols="12"
-        md="6"
-        lg="3"
-      >
-        <material-stat-card v-bind="attrs">
-          <template #actions>
-            <v-icon
-              class="mr-2"
-              small
-              v-text="actionIcon"
+    <v-fade-transition mode="out-in">
+      <v-row>
+        <!-- <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://picsum.photos/350/165?random"
+              height="125"
+              class="grey darken-4"
             />
-            <div class="text-truncate">
-              {{ actionText }}
-            </div>
-          </template>
-        </material-stat-card>
-      </v-col>
+            <v-card-title class="text-h6">
+              height
+            </v-card-title>
+          </v-card>
+        </v-col>
 
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <material-card
-          color="orange"
-          full-header
-        >
-          <template #heading>
-            <div class="pa-8 white--text">
-              <div class="text-h4 font-weight-light">
-                Employees Stats
-              </div>
-              <div class="text-caption">
-                New employees on 15th September, 2016
-              </div>
-            </div>
-          </template>
-          <v-card-text>
-            <v-data-table
-              :headers="headers"
-              :items="items"
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://picsum.photos/350/165?random"
+              height="125"
+              contain
+              class="grey darken-4"
             />
-          </v-card-text>
-        </material-card>
-      </v-col>
+            <v-card-title class="text-h6">
+              height with contain
+            </v-card-title>
+          </v-card>
+        </v-col> -->
 
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <material-card
-          color="accent"
-          full-header
-        >
-          <template #heading>
-            <v-tabs
-              v-model="tabs"
-              background-color="transparent"
-              slider-color="white"
-              class="pa-8"
-            >
-              <span
-                class="subheading font-weight-light mx-3"
-                style="align-self: center"
-              >Tasks:</span>
-              <v-tab class="mr-3">
-                <v-icon class="mr-2">
-                  mdi-bug
-                </v-icon>
-                Bugs
-              </v-tab>
-              <v-tab class="mr-3">
-                <v-icon class="mr-2">
-                  mdi-code-tags
-                </v-icon>
-                Website
-              </v-tab>
-              <v-tab>
-                <v-icon class="mr-2">
-                  mdi-cloud
-                </v-icon>
-                Server
-              </v-tab>
-            </v-tabs>
-          </template>
-          <v-tabs-items
-            v-model="tabs"
-            background-color="transparent"
-          >
-            <v-tab-item
-              v-for="n in 3"
-              :key="n"
-            >
-              <v-card-text>
-                <template v-for="(task, i) in tasks[tabs]">
-                  <v-row
-                    :key="i"
-                    align="center"
-                    class="flex-nowrap"
-                  >
-                    <v-col cols="1">
-                      <v-list-item-action>
-                        <v-simple-checkbox
-                          v-model="task.value"
-                          color="secondary"
-                        />
-                      </v-list-item-action>
-                    </v-col>
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://drive.google.com/uc?id=1HL8w8-r-XnEgS_19JdzYGOJCXDDOpqA-"
+              max-height="1250"
+              class="grey darken-4"
+            />
+            <v-card-title class="text-h6">
+              max-height
+            </v-card-title>
+          </v-card>
+        </v-col>
 
-                    <v-col
-                      class="font-weight-light"
-                      cols="8"
-                      v-text="task.text"
-                    />
-
-                    <v-col
-                      cols="auto"
-                      class="text-right"
-                    >
-                      <v-icon class="mx-1">
-                        mdi-pencil
-                      </v-icon>
-
-                      <v-icon
-                        class="mx-1"
-                        color="error"
-                      >
-                        mdi-close
-                      </v-icon>
-                    </v-col>
-                  </v-row>
-                </template>
-              </v-card-text>
-            </v-tab-item>
-          </v-tabs-items>
-        </material-card>
-      </v-col>
-    </v-row> -->
+        <v-col cols="6">
+          <v-card>
+            <v-img
+              src="https://wokolmotoryzacji.pl/wp-content/uploads/2019/12/35GT-RR_w_R2-1024x576.jpeg"
+              max-height="1250"
+              contain
+              class="grey darken-4"
+            />
+            <v-card-title class="text-h6">
+              max-height with contain
+            </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-fade-transition>
   </v-container>
 </template>
 
 <script>
-  // const { google } = require('googleapis')
-  // const CLIENT_ID = '169829791352-r5g9oogt9u41me3e4a4k0iuaaivmgta8.apps.googleusercontent.com'
-  // const CLIENT_SECRET = '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'
-  // const REDIRECT_URI = 'https://developers.google.com/oauthplayground'
-
-  // const REFRESH_TOKEN = '1//04fQJpR58q1hJCgYIARAAGAQSNgF-L9Ir-tjjg5UpgOiWbdDTRd6YCAeg9AFsbi48wluHZtuxyeiDgJXqNKK2hMsLJtIX2JKquA'
-
-  // const oauth2Client = new google.auth.OAuth2(
-  //   CLIENT_ID,
-  //   CLIENT_SECRET,
-  //   REDIRECT_URI,
-  // )
-
-  // oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
-
-  // const drive = google.drive({
-  //   version: 'v3',
-  //   auth: oauth2Client,
-  // })
   export default {
-    mounted () {
-      console.log('mounted')
-      // The ID of your GCS bucket
-const bucketName = 'Edit';
-
-// The full path of your file inside the GCS bucket, e.g. 'yourFile.jpg' or 'folder1/folder2/yourFile.jpg'
-const fileName = 'https://drive.google.com/drive/folders/1D9gMtEt6DMGJUlyM74cip6sYSAxV2Uvy';
-
-// Imports the Google Cloud client library
-const {Storage} = require('@google-cloud/storage');
-
-// Creates a client
-const storage = new Storage();
-
-async function generateV4ReadSignedUrl() {
-  // These options will allow temporary read access to the file
-  const options = {
-    version: 'v4',
-    action: 'read',
-    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
-  };
-
-  // Get a v4 signed URL for reading the file
-  const [url] = await storage
-    .bucket(bucketName)
-    .file(fileName)
-    .getSignedUrl(options);
-
-  console.log('Generated GET signed URL:');
-  console.log(url);
-  console.log('You can use this URL with any user agent, for example:');
-  console.log(`curl '${url}'`);
-}
-
-generateV4ReadSignedUrl().catch(console.error);
-    
+    components: {
+    },
+    data () {
+      //
     },
   }
+
 </script>
