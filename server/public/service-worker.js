2b8a318120d4ad4582e2c1f7144d15a32481555e
Michał Jaroń <64842623+streetmarker@users.noreply.github.com>
2022-05-13 22:58:48 +0200

    quick fix

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -62,27 +62,22 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('push', function(event) {
-    if (!(self.Notification && self.Notification.permission === 'granted')) {
-      return;
-    }
-  
-    var data = {};
-    if (event.data) {
-      data = event.data.json();
-    }
-    var title = data.title || "Something Has Happened";
-    var message = data.message || "Here's something you might want to check out.";
-    var icon = "images/new-notification.png";
-  
-    var notification = new self.Notification(title, {
-      body: message,
-      tag: 'simple-push-demo-notification',
-      icon: icon
-    });
-  
-    notification.addEventListener('click', function() {
-      if (clients.openWindow) {
-        clients.openWindow('https://example.blog.com/2015/03/04/something-new.html');
-      }
-    });
-  });
+    // Keep the service worker alive until the notification is created.
+    event.waitUntil(
+      getEndpoint()
+      .then(function(endpoint) {
+        // Retrieve the textual payload from the server using a GET request.
+        // We are using the endpoint as an unique ID of the user for simplicity.
+        return fetch('./getPayload?endpoint=' + endpoint);
+      })
+      .then(function(response) {
+        return response.text();
+      })
+      .then(function(payload) {
+        // Show a notification with title 'ServiceWorker Cookbook' and use the payload
+        // as the body.
+        self.registration.showNotification('ServiceWorker Cookbook', {
+          body: payload,
+        });
+      }))
+    })
